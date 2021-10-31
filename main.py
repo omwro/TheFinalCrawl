212c6bf476843743868f827b150140d82ccc64d1
Omer Erdem <omer.erdem@hva.nl>
2021-10-31 11:43:42 +0100

    logs added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 import time
 from discord import Webhook, RequestsWebhookAdapter
-from env import DISCORD_WEBHOOK_URL
+from env import DISCORD_WEBHOOK_URL, DISCORD_LOG_WEBHOOK_URL
 
 PRODUCTS_JSON_URL = 'https://finalmouse.com/products.json'
 START_URL = 'https://finalmouse.com/collections/museum/products/'
@@ -10,17 +10,19 @@ has_found_new_product = False
 
 
 def main():
-    notify_discord("Starting up THE FINAL CRAWL!")
+    notify_discord_log("Starting up THE FINAL CRAWL!")
     product_handles = pd.read_csv('product-handles.csv')['handle']
 
     while True:
+        start_time = time.time()
         df_products = init()
 
         if not has_found_new_product:
             df_products.apply(lambda p: check_if_new_product(product_handles, p), axis=1)
 
         df_products.apply(lambda p: check_stock(p), axis=1)
-
+        end_time = time.time() - start_time
+        notify_discord_log("[{}] Check completed in {}ms".format(time.strftime("%H:%M:%S"), int(round(end_time, 3) * 1_000)))
         time.sleep(5)
 
 
@@ -47,5 +49,12 @@ def notify_discord(message):
     Webhook.from_url(DISCORD_WEBHOOK_URL, adapter=RequestsWebhookAdapter()).send(message)
 
 
+def notify_discord_log(message):
+    Webhook.from_url(DISCORD_LOG_WEBHOOK_URL, adapter=RequestsWebhookAdapter()).send(message)
+
+
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    finally:
+        notify_discord_log("[{}] EXCEPTION OCCURRED!".format(time.strftime("%H:%M:%S")))
